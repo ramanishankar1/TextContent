612862fc7113684af1ba7545ffd7f60c6449c8d3
RamaniShankar1 <ramshankarjan1222@gmail.com>
2022-11-02 19:50:28 +0530

    Compl;eted

---
+++ main.js
@@ -0,0 +1,7 @@
+var myName = document.getElementById('name')
+console.log(myName.innerHTML);
+console.log(myName.innerText);
+console.log(myName.textContent);
+var input = document.getElementById('input')
+console.log(input.value);
+
